c8734096e3421b96c69aa037faf0a877eb9211d5
Hasib Al Rashid <cadethasib3091@gmail.com>
2020-12-20 22:05:18 +0600

    Connecting and Intializing Firebase and Sign In

--- src/App.js
+++ src/App.js
@@ -1,9 +1,17 @@
+import React from 'react';
 import './App.css';
+import { useAuthState } from 'react-firebase-hooks/auth';
+import './firebase';
+import { auth } from './firebase';
+import { firestore } from "./firebase";
+import SignIn from './SignIn';
 
 function App() {
+	const [user] = useAuthState(auth);
+
 	return (
 		<div className="App">
-			
+			{user ? <h1>Chat Room</h1> : <SignIn /> }
 		</div>
 	);
 }
---
+++ src/firebase.js
@@ -0,0 +1,17 @@
+import firebase from 'firebase/app';
+import 'firebase/firestore';
+import 'firebase/auth';
+import { useCollectionData } from 'react-firebase-hooks/firestore';
+
+firebase.initializeApp({
+	apiKey: "AIzaSyDJXO2nsXN_a40ZjxAEsbuquWTZZaZ0wVQ",
+    authDomain: "facebook-clone-bcdb2.firebaseapp.com",
+    projectId: "facebook-clone-bcdb2",
+    storageBucket: "facebook-clone-bcdb2.appspot.com",
+    messagingSenderId: "826018512732",
+    appId: "1:826018512732:web:f8ca01fec37c1d891d0b99",
+    measurementId: "G-6BCQ90VTPY"
+})
+
+export const auth = firebase.auth();
+export const firestore = firebase.firestore();
