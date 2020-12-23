1067b168ea8cd2fd5777e526159712c054491b18
Hasib Al Rashid <cadethasib3091@gmail.com>
2020-12-23 23:33:37 +0600

    init

--- src/components/MessageSender/MessageSender.js
+++ src/components/MessageSender/MessageSender.js
@@ -3,6 +3,9 @@ import { Avatar } from "@material-ui/core";
 import { useAuthState } from "react-firebase-hooks/auth";
 import "../../firebase";
 import { auth } from "../../firebase";
+import VideocamIcon from "@material-ui/icons/Videocam";
+import PhotoLibraryIcon from "@material-ui/icons/PhotoLibrary";
+import MoodIcon from "@material-ui/icons/Mood";
 
 function MessageSender() {
     const [user] = useAuthState(auth);
@@ -24,11 +27,20 @@ function MessageSender() {
             </div>
 
             <div className="messageSender__bottom">
-                <div className="messageSender__option"></div>
+                <div className="messageSender__option">
+                    <VideocamIcon style={{ color: "crimson" }} />
+                    <h3>Live Video</h3>
+                </div>
 
-                <div className="messageSender__option"></div>
+                <div className="messageSender__option">
+                    <PhotoLibraryIcon style={{ color: "green" }} />
+                    <h3>Photo / Video</h3>
+                </div>
 
-                <div className="messageSender__option"></div>
+                <div className="messageSender__option">
+                    <MoodIcon style={{ color: "#ffe700" }} />
+                    <h3>Feeling / Activity</h3>
+                </div>
             </div>
         </div>
     );
